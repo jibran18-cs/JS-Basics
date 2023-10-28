5ec4639b549d89b3079f9b85d585ecb3a86113fd
Mohammad Jibran <115047098+jibran18-cs@users.noreply.github.com>
2023-10-28 13:49:24 +0000

    Updated

---
+++ 01_Basics/08_date.js
@@ -0,0 +1,36 @@
+
+const date= new Date()
+//These are diiferent mehtods to represent date
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toLocaleDateString())
+// console.log(date.getUTCFullYear())
+// console.log(date.getUTCDate())
+// console.log(date.getUTCDay())
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getTime())
+// console.log(typeof date);
+
+// Dates declaration
+
+// const anotherDate= new Date(2023,0,10)
+// const anotherDate= new Date(2023, 0, 10, 5, 3)
+// const anotherDate= new Date('2023-01-10')
+// const anotherDate= new Date('02-14-2023')
+// console.log(anotherDate.toLocaleString())
+
+//Time stamps
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
